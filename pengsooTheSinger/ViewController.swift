3533f8e86ce6e7de61d2466dcd376618843cf8c8
rhkwj <rhkwj@naver.com>
2020-02-05 21:13:17 +0800

    copy the music note and before do story board

--- pengsooTheSinger/ViewController.swift
+++ pengsooTheSinger/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var musicNote: UIImageView!
+    @IBOutlet weak var musicNote2: UIImageView!
+    @IBOutlet weak var pengSoo: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        musicNote.alpha = 0
+        musicNote2.alpha = 0
     }
+}
 
-
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //print(scrollView.contentOffset)
+        
+        //When pengsoo arrived at (0,0)
+        //Then show the music note
+        
+        if scrollView.contentOffset.x == 0 {
+//            musicNote?.isHidden = false
+//            musicNote2?.isHidden = false
+//How to make animation
+//https://stackoverflow.com/questions/6177393/how-to-add-animation-while-changing-the-hidden-mode-of-a-uiview
+            
+            UIView.animate(withDuration: 1.0, delay: 0, options: [], animations: {
+                self.musicNote?.alpha = 1
+                self.musicNote2?.alpha = 1
+            }, completion: nil)
+        } else {
+            UIView.animate(withDuration: 1.0, delay: 0, options: [], animations: {
+                self.musicNote?.alpha = 0
+                self.musicNote2?.alpha = 0
+            }, completion: nil)
+        }
+    }
 }
 
